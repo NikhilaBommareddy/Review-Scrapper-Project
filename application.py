9749a3195485c07c6a7dd0ecd107b29722dad246
Nikhila Bommareddy <nikhilaashokreddy@gmail.com>
2023-07-03 12:01:00 -0400

    Updated

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
 @app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
@@ -83,5 +84,5 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-     app.run(host='127.0.0.1', port=3000, debug=True)
+     app.run(host='0.0.0.0',debug=True)
 	# app.run(debug=True)
